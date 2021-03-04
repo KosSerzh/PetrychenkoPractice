82cf0192e99f2eba3f0f947cccf1cd51b0c672fc
Kos <example@gmail.com>
2021-03-04 15:07:17 +0200

    FIrstCommit

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+
+"use strict";
+// a = 15;
+// console.log (a);
+// let number = 5 ;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log (number);
+
+// const obj = {a:50};
+//  obj.a = 10;
+
+//  console.log (obj);
+// let number = 4.6;
+
+// console.log (-4/0);
+// console.log( 'string*5');
+
+// const person = 'Kos' ;
+
+// const bool = true / false ;
+
+// let und ;
+// console.log (und);
+
+// const obj = {
+//     name: 'Kos',
+//     age: 30,
+//     isMarried: false
+// };
+// console.log (obj.name);
+
+// let arr = ['orange.jpg','plum.png',6 ,'apple.bmp', {}, ['unnamed fruite'], ]
+// console.log (arr [5]);
+// alert ('Hello');
+
+// const result = confirm('are you here?');
+// console.log (result);
+
+// const answers = [];
+// answers [0]= prompt ('What is your name','');
+// answers [1]= prompt ('What is your surname','');
+// answers [2]= prompt ('What is your age','');
+// console.log(typeof(answers));
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user =  'Kos';
+alert (`helo ${user}`);
